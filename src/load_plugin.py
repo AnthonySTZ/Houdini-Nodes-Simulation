90451f8b2ac0614e9f51f05fb049c2cac335310f
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-22 16:35:47 +0100

    laod math and particle module

--- src/load_plugin.py
+++ src/load_plugin.py
@@ -5,11 +5,15 @@ plugin_path = os.environ["HOUDINI_SPRING_NODES_PATH"]
 
 sys.path.insert(0, plugin_path)
 
+import spring.math as math
+import spring.particle as particle
 import spring.simulation as simulation
 import spring.interface as interface
 import spring.plugin as plugin
 from importlib import reload
 
+reload(math)
+reload(particle)
 reload(simulation)
 reload(interface)
 reload(plugin)
