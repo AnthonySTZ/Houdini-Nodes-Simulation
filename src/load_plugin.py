10c4b45f9e48074dd3d2b1b7f8db73ce28d7ccd8
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-22 17:07:02 +0100

    load all modules

--- src/load_plugin.py
+++ src/load_plugin.py
@@ -5,15 +5,21 @@ plugin_path = os.environ["HOUDINI_SPRING_NODES_PATH"]
 
 sys.path.insert(0, plugin_path)
 
+import spring.nodes as nodes
 import spring.math as math
 import spring.particle as particle
+import spring.spring as spring
+import spring.world as world
 import spring.simulation as simulation
 import spring.interface as interface
 import spring.plugin as plugin
 from importlib import reload
 
+reload(nodes)
 reload(math)
 reload(particle)
+reload(spring)
+reload(world)
 reload(simulation)
 reload(interface)
 reload(plugin)
