3738cf4dcbd3b269530a3eaeffe92930fb6674b4
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-22 17:18:01 +0100

    add node in particle

--- src/spring/particle.py
+++ src/spring/particle.py
@@ -3,6 +3,7 @@ from spring.math import Vector2
 
 class Particle:
     def __init__(self, node) -> None:
+        self.node = node
         self.position = Vector2(node.position().x, node.position().y)
         self.velocity = Vector2(0, 0)
         self.acceleration = Vector2(0, 0)
@@ -12,6 +13,9 @@ class Particle:
         self.acceleration += force
 
     def update(self, dt: float) -> None:
+        if self.is_anchor:
+            return
         self.velocity += self.acceleration * dt
         self.position += self.velocity * dt
         self.acceleration *= 0
+        self.node.setPosition(self.position.x, self.position.y)
