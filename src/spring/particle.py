46bbe18d064507b8a2b356753e4f8bca8c153470
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2024-12-22 17:06:51 +0100

    use hou.Node instead of x, y

--- src/spring/particle.py
+++ src/spring/particle.py
@@ -2,8 +2,8 @@ from spring.math import Vector2
 
 
 class Particle:
-    def __init__(self, x: float, y: float) -> None:
-        self.position = Vector2(x, y)
+    def __init__(self, node) -> None:
+        self.position = Vector2(node.position().x, node.position().y)
         self.velocity = Vector2(0, 0)
         self.acceleration = Vector2(0, 0)
         self.is_anchor = False
